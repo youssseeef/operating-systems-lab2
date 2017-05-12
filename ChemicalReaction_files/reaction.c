a445a541db5257e6d52958ccf83db81c58d4bb57
Amr Elsekilly <amr.e.dev@gmail.com>
2017-05-12 20:52:10 +0200

    initialized the structure

--- ChemicalReaction_files/reaction.c
+++ ChemicalReaction_files/reaction.c
@@ -6,10 +6,19 @@
 // appropriate within reaction_o()/reaction_h().
 void make_water();
 
-
+// Initialize the reaction object
 void reaction_init(struct reaction *reaction)
 {
-	// FILL ME IN
+  // clear the counters
+  reaction->countO = 0;
+  reaction->countH = 0;
+
+  // init the mutex
+  pthread_mutex_init(&reaction->ourMutex, 0);
+
+  // init the condition variables
+  pthread_cond_init(&reaction->condO, 0);
+  pthread_cond_init(&reaction->condP, 0);
 }
 
 
@@ -23,5 +32,3 @@ void reaction_o(struct reaction *reaction)
 {
 	// FILL ME IN
 }
-
-
