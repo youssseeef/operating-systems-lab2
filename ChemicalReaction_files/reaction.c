765ff90ee2444ebde1fe3726aa947d9e5afacd37
Youssef Salama <youssefhanii@gmail.com>
2017-05-12 23:25:08 +0200

    started getting some outcome

--- ChemicalReaction_files/reaction.c
+++ ChemicalReaction_files/reaction.c
@@ -9,60 +9,61 @@ void make_water();
 // Initialize the reaction object
 void reaction_init(struct reaction *reaction)
 {
-  // clear the counters
-  reaction->countO = 0;
-  reaction->countH = 0;
+    // clear the counters
+    reaction->countO = 0;
+    reaction->countH = 0;
 
-  // init the mutex
-  pthread_mutex_init(&reaction->ourMutex, 0);
+    // init the mutex
+    pthread_mutex_init(&reaction->ourMutex, 0);
 
-  // init the condition variables
-  pthread_cond_init(&reaction->condO, 0);
-  pthread_cond_init(&reaction->condH, 0);
+    // init the condition variables
+    pthread_cond_init(&reaction->condO, 0);
+    pthread_cond_init(&reaction->condH, 0);
 }
-
 
 void reaction_h(struct reaction *reaction)
 {
-  // lock the critical section
-  pthread_mutex_lock(&reaction->ourMutex);
+    // lock the critical section
+    pthread_mutex_lock(&reaction->ourMutex);
 
-  // check the count of H atoms
-  if (reaction->countH == 2) {
-    // signal the condH
-    pthread_cond_wait(&reaction->condH, &reaction->ourMutex);
-    // check for the o count
-    if (reaction->countO == 1) {
-      make_water();
+    // check the count of H atoms
+    while (reaction->countH != 2) pthread_cond_wait(&reaction->condH, &reaction->ourMutex);
+    // signal the condH			    // check for the o count
+    reaction->countO += 1;
+    pthread_cond_signal(&reaction->condO);
+    if (reaction->countO == 1)
+    {
+	make_water();
     }
-  } else {
-    // create a new H atom
-    ++reaction->countH;
-  }
+    else
+    {
+	// create a new H atom
+	//++reaction->countH;
+    }
 
-  // realease access to the critical section
-  pthread_mutex_unlock(&reaction->ourMutex);
+    // realease access to the critical section
+    pthread_mutex_unlock(&reaction->ourMutex);
 }
-
 
 void reaction_o(struct reaction *reaction)
 {
-	// lock the critical section
-  pthread_mutex_lock(&reaction->ourMutex);
+    pthread_mutex_lock(&reaction->ourMutex);
 
-  // check the count of O atoms
-  if (reaction->countO == 1) {
-    // signal the condO
-    pthread_cond_wait(&reaction->condO, &reaction->ourMutex);
-    // check for the H count
-    if (reaction->countH == 2) {
-      make_water();
+    // check the count of H atoms
+    while (reaction->countO != 1) pthread_cond_wait(&reaction->condO, &reaction->ourMutex);
+    // signal the condH			    // check for the o count
+    reaction->countO += 1;
+    pthread_cond_signal(&reaction->condH);
+    if (reaction->countH == 1)
+    {
+	make_water();
     }
-  } else {
-    // create a new H atom
-    ++reaction->countO;
-  }
+    else
+    {
+	// create a new H atom
+	//++reaction->countH;
+    }
 
-  // realease access to the critical section
-  pthread_mutex_unlock(&reaction->ourMutex);
+    // realease access to the critical section
+    pthread_mutex_unlock(&reaction->ourMutex);
 }
